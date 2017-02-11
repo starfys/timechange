d9cf1bd0fbd575ee8fb3730e13b7eed3efc2b2dc
Steven Sheffey <stevensheffey4@gmail.com>
2017-02-11 00:03:31 -0600

    Proof of concept to generate FFT-based images

--- timechange/__init__.py
+++ timechange/__init__.py
@@ -7,7 +7,6 @@ import pandas
 class TimeChange:
     def __init__(self, model=None):
         """Constructor"""
-        print("Constructor called")
         self.training_files = defaultdict(set)
         self.model = model
     def add_training_file(self, label, filename):
@@ -28,14 +27,33 @@ class TimeChange:
             self.training_files[label].remove(filename)
         except KeyError:
             print("File {} not found under label {}".format(filename, label), file=sys.stderr)
-    def time_series_to_image(time_series, method="fft"):
+    def extract_features(self, time_series, method="fft", data_size=None):
         """Extracts features from a time series or array of time series and outputs an image
         Keyword arguments:
         time_series -- A numpy array or array of numpy arrays representing the time series data
         """
+        #Fix data size
+        if data_size is None:
+            data_size = time_series.shape[1]
         #TODO: implement this
         if method == "fft":
-            return np.abs(np.fft.fft(time_series))
+            features = np.abs(np.fft.rfft(time_series, n=data_size))
+            return features / np.max(features)
         else:
             print("Feature extraction method {} invalid.".format(method), file=sys.stderr)
             return None
+    def read_csv(self, filename, *args, **kwargs):
+        """Reads a time-series data csv into a file
+        Keyword arguments:
+        filename -- filename to read into
+        *args -- Extra args to pass the pandas parser
+        """
+        return pandas.read_csv(filename, *args, **kwargs).as_matrix().T
+    def get_csv_columns(self, filename, *args, **kwargs):
+        """Reads a csv file and returns the column names
+        Keyword arguments:
+        filename -- filename to read into
+        *args -- Extra args to pass the pandas parser
+        """
+        return pandas.read_csv(filename, nrows=1, *args, **kwargs).columns
+
