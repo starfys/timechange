115d974a60b2597fe26da9301f91b90c7f85c7fa
John Ford <jrf2m@mtmail.mtsu.edu>
2017-02-11 21:57:12 -0600

    handle stripping the same columns from multiple files

--- tests.py
+++ tests.py
@@ -3,18 +3,7 @@ import sys
 import os
 import timechange
 
-tc = timechange.TimeChange()
-if len(sys.argv) < 2:
-    print("Need a directory (not an individual .csv)")
-    exit()
-for file_id, csv_filename in enumerate(os.listdir(sys.argv[1])):
-    print(csv_filename)
-    csv_filename = os.path.join(sys.argv[1], csv_filename)
-    data_columns = list(tc.get_csv_columns(csv_filename))
-    print("Found {} columns".format(len(data_columns)))
-    for index, col_name in enumerate(data_columns):
-        print("{}: {}".format(index, col_name))
-
+def query_columns(data_columns):
     answer = ""
     print("Should we use all columns? (yes/no): ")
     answer = input().rstrip()
@@ -23,7 +12,8 @@ for file_id, csv_filename in enumerate(os.listdir(sys.argv[1])):
         print("Should we use all columns? (yes/no): ")
         answer = input().rstrip()
     if answer == "no":
-        print("What columns should be removed? (enter a comma separated list of column names): ")
+        print("What columns should be removed?")
+        print("(enter a comma separated list of column names): ")
         answer = input().rstrip().split(',')
         while True:
             backup_columns = data_columns
@@ -33,12 +23,57 @@ for file_id, csv_filename in enumerate(os.listdir(sys.argv[1])):
                 break
             except Exception as e:
                 data_columns = backup_columns
-                print("Invalid answer - perhaps you gave an index number not a column header name?")
-                print("What columns should be removed? (enter a comma separated list of column numbers): ")
+                print("Invalid answer. Perhaps you gave an index number not a column header name.")
+                print("What columns should be removed?")
+                print("(enter a comma separated list of column numbers): ")
                 answer = input().rstrip().split(',')
         print("The data now will only use these columns:")
         for index, col_name in enumerate(data_columns):
             print("{}: {}".format(index, col_name))
+    return data_columns
+
+tc = timechange.TimeChange()
+if len(sys.argv) < 2:
+    print("Need a directory (not an individual .csv)")
+    exit()
+
+input_directory = sys.argv[1]
+print("Found the following files in %s" % input_directory)
+print(str(os.listdir(input_directory)))
+
+individual_strip = True
+group_stripped_columns = []
+headersList = []
+for file_id, csv_filename in enumerate(os.listdir(input_directory)):
+    csv_filename = os.path.join(sys.argv[1], csv_filename)
+    data_columns = list(tc.get_csv_columns(csv_filename))
+    headersList += [data_columns]
+
+if all(headers==headersList[0] for headers in headersList):
+    print("Headers identical in all files.")
+    print("Do you want to strip columns for each file individually?")
+    answer = ""
+    answer = input().rstrip()
+    while answer not in ("yes", "no"):
+        print("Invalid answer")
+        print("Should we use all columns? (yes/no): ")
+        answer = input().rstrip()
+    if answer == "no":
+        individual_strip = False
+        group_strip_columns = query_columns(data_columns)
+
+for file_id, csv_filename in enumerate(os.listdir(input_directory)):
+    print(csv_filename)
+    csv_filename = os.path.join(sys.argv[1], csv_filename)
+    data_columns = list(tc.get_csv_columns(csv_filename))
+    print("Found {} columns".format(len(data_columns)))
+    for index, col_name in enumerate(data_columns):
+        print("{}: {}".format(index, col_name))
+
+    if individual_strip:
+        data_columns = query_columns(data_columns)
+    else:
+        data_columns = group_strip_columns
 
     data = tc.read_csv(csv_filename, usecols=data_columns)
 
