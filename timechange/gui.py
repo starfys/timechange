b6ae8d5b93bfecc7cffdf2df6e88e8ba984a42b6
Steven Sheffey <starfys@users.noreply.github.com>
2017-04-11 12:56:54 -0500

    Integration (#20)
    * Did a few fixes with the main class and a bit more integration with the gui
    
    * Added the ability to display training statistics to the training button. Training currently runs in the main thread
    
    * Fix for threading

--- timechange/gui.py
+++ timechange/gui.py
@@ -10,6 +10,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 import timechange
 from threading import *
 
+
 class CheckBoxSet(Frame):
     def __init__(self, parent, picks=[], side=LEFT, anchor=W):
         Frame.__init__(self, parent)
